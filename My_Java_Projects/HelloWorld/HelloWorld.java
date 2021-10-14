f2e3b27e7f9afc63cf2295b054eeb7e8bbe51493
CindyChenGitHub <blueapple.phoebe@gmail.com>
2021-10-14 10:45:53 -0400

    20211014

---
+++ My_Java_Projects/HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+* Copyright (C), 2021-2031, Yolanda
+* FileName: HelloWorld.java
+* 类的详细说明
+*
+* @author  Yolanda (Yue) C.
+* @Date    2021-10-13
+* @version 1.00
+*/
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
